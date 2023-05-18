4785b60b87ffd2d9e529219fe8926a11de6a3b28
jbamlee <dev.jbam@gmail.com>
2023-05-18 17:02:51 +0900

    fix: find message by key instead of using index

--- src/collector/log-finder/txHistoryLF.ts
+++ src/collector/log-finder/txHistoryLF.ts
@@ -1,8 +1,39 @@
-import { createReturningLogFinder, ReturningLogFinderMapper } from '@terra-money/log-finder'
+import { Attribute, createReturningLogFinder, ReturningLogFinderMapper } from '@terra-money/log-finder'
 import { trimAssets, addMinus } from 'lib/utils'
 import { TxHistoryTransformed } from 'types'
 import logRules from './log-rules'
 
+type asset = {token: string; amount: string}
+
+enum SwapMessageKey {
+  contractAddress = '_contract_address',
+  action = 'action',
+  sender = 'sender',
+  receiver = 'receiver',
+  offerAsset = 'offer_asset',
+  askAsset = 'ask_asset',
+  offerAmount = 'offer_amount',
+  returnAmount = 'return_amount',
+  spreadAmount = 'spread_amount',
+  commissionAmount = 'commission_amount',
+}
+enum ProvideMessageKey {
+  contractAddress= '_contract_address',
+  action= 'action',
+  sender= 'sender',
+  receiver= 'receiver',
+  assets= 'assets',
+  share= 'share'
+}
+
+enum WithdrawMessageKey {
+contractAddress = '_contract_address'	,
+action = 'action'	,
+refundAssets = 'refund_assets',
+sender = 'sender'	,
+withdrawnShare = 'withdrawn_share'	
+
+}
 function createLegacySPWFinder(
   pairAddresses: Record<string, boolean>
 ): ReturningLogFinderMapper<TxHistoryTransformed> {
@@ -67,16 +98,19 @@ function createLegacySPWFinder(
       let share = '0'
 
       if (action === 'swap') {
-        assets[0].token = match[4].value
-        assets[0].amount = match[6].value
-        assets[1].token = match[5].value
-        assets[1].amount = addMinus(match[7].value)
+        assets = swapMatchToAssets(match)
       } else if (action === 'provide_liquidity') {
-        assets = trimAssets(match[4].value, true)
-        share = match[5].value
+        const assetMsg = match.find((m) => m.key === ProvideMessageKey.assets)
+        if (assetMsg){
+          assets = trimAssets(assetMsg.value, true)
+        }
+        share = matchesToShare(match, ProvideMessageKey.share)
       } else if (action === 'withdraw_liquidity') {
-        assets = trimAssets(match[4].value, false)
-        share = match[3].value
+        const assetMsg = match.find((m) => m.key === WithdrawMessageKey.refundAssets)
+        if (assetMsg){
+          assets = trimAssets(assetMsg.value, true)
+        }
+        share = matchesToShare(match, WithdrawMessageKey.withdrawnShare)
       }
 
       const transformed = {
@@ -92,4 +126,45 @@ function createLegacySPWFinder(
   })
 }
 
-export const createSPWFinder =  process.env.TERRA_CHAIN_ID === "columbus-4" ? createLegacySPWFinder : createNewSPWFinder
+function swapMatchToAssets(matches: Attribute[]): asset[] {
+  const assets: asset[] = [
+    {
+      token: '',
+      amount: '',
+    },
+    {
+      token: '',
+      amount: '',
+    },
+  ]
+
+  matches.forEach((match) => {
+    if (match.key === SwapMessageKey.offerAsset) {
+      assets[0].token = match.value
+    }
+    if (match.key === SwapMessageKey.offerAmount) {
+      assets[0].amount = match.value
+    }
+    if (match.key === SwapMessageKey.askAsset) {
+      assets[1].token = match.value
+    }
+    if (match.key === SwapMessageKey.returnAmount) {
+      assets[1].amount = "-" + match.value
+    }
+  })
+
+  return assets
+}
+
+function matchesToShare(matched: Attribute[],  targetKey: string): string {
+  let share = '0'
+  matched.forEach((match) => {
+    if (match.key === targetKey) {
+      share = match.value
+    }
+  })
+
+  return share
+}
+
+export const createSPWFinder = process.env.TERRA_CHAIN_ID === "columbus-4" ? createLegacySPWFinder : createNewSPWFinder
