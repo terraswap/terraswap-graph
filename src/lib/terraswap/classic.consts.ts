78024800a19b7b84046869ae9c910f730fe55c76
jbamlee <dev.jbam@gmail.com>
2024-02-16 16:22:03 +0900

    feat: apply BASE token tax on classic network

--- src/lib/terraswap/classic.consts.ts
+++ src/lib/terraswap/classic.consts.ts
@@ -1,6 +1,6 @@
 export const ClassicReceiverFeeAppliedTokenSet: Set<string> = new Set([
-"terra1zkhwtm4a559emekwj7z4vklzqupgjyad8ncpwvav38y5ef6g5tjse7ceus",
-"terra1naaldj58aerjvqzulrnfpeph60pjqlyp60gqryup8g4djy4cn4nqwm08c3"
+    "terra1zkhwtm4a559emekwj7z4vklzqupgjyad8ncpwvav38y5ef6g5tjse7ceus",
+    "terra1naaldj58aerjvqzulrnfpeph60pjqlyp60gqryup8g4djy4cn4nqwm08c3"
 ])
 
 export const ClassicReceiverFeeAppliedPairSet: Set<string> = new Set([
@@ -8,3 +8,17 @@ export const ClassicReceiverFeeAppliedPairSet: Set<string> = new Set([
     "terra1anwgp97zfn6zsz9t5vgtan4s09n24khx0twu3a550m4dsxj699pq8mg5yz",
     "terra17tq86waugtxrupjp3yz2fran6ghfcsdt35j257dk7tznljlkm9rq49j7ap"
 ])
+
+interface OddTokenHandlingInfo {
+    feeRatio: string
+    appliedHeight: number
+    action: (a: string)=> boolean
+}
+
+const CLASSIC_BASE_TOKEN = "terra1uewxz67jhhhs2tj97pfm2egtk7zqxuhenm4y4m"
+const APPLIED_HEIGHT = 16746830
+export const ClassicOddTokenHandlerMap: Map<string, OddTokenHandlingInfo> = new Map([[CLASSIC_BASE_TOKEN, {
+    feeRatio: "0.048",
+    appliedHeight: APPLIED_HEIGHT,
+    action: (a: string) => a === "send"
+}]])
